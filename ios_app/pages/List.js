d15aa4680573f0d42f50171515a8c9d2cf6d5879
huangjipeng <1319639755@qq.com>
2021-12-27 13:17:37 +0800

    ListPage module fetch data development.

--- ios_app/pages/List.js
+++ ios_app/pages/List.js
@@ -8,7 +8,44 @@ import {
   Text
 } from 'react-native';
 
+import ListModel from '../models/List'
+
+const listModel = new ListModel()
+
 class ListPage extends Component {
+
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      fieldData: []
+    }
+  }
+
+  getCourseFields () {
+    listModel.getCourseFields().then(res => {
+      const data = res.result
+
+      this.setState({
+        fieldData: [{ field: 'all', field_name: '全部课程' }].concat(data)
+      })
+      console.log(this.state.fieldData)
+    })
+  }
+
+  getCourses (field) {
+    listModel.getCourses(field).then(res => {
+      const data = res.result
+
+      console.log(data)
+    })
+  }
+
+  componentDidMount () {
+    this.getCourseFields()
+    this.getCourses('1')
+  }
+
   render() {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
